1bb784852b550dac96b51ca19bfde7aaea91da44
Antonio G Papa <antoniogpapa@gmail.com>
2017-11-11 20:07:48 -0500

    Function that takes query, gives result
    Gives the corresponding intent and entity. Entity will be blank if one
    was not detected.

---
+++ luis.py
@@ -0,0 +1,17 @@
+import urllib
+import json
+
+def query(q):
+    base_url = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/daf031ac-3b4e-4508-b0de-91f76fd0348f?subscription-key=7ec7b1d0fbb24d01bd1e20841e00bb15&spellCheck=true&verbose=true&timezoneOffset=-5.0&q="
+
+    url = base_url + q
+
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+    
+    intent = data["topScoringIntent"]["intent"]
+    try:
+        entity = data["entities"][0]["entity"]
+    except:
+        entity = ""
+    return (intent, entity)
